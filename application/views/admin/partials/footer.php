2e41876dd5ef4e0b09aba0ccadc533ad6e1fbbd3
Ari Putra Utama <arieputrautama1998@gmail.com>
2024-07-24 21:14:03 +0700

    Update

---
+++ application/views/admin/partials/footer.php
@@ -0,0 +1,6 @@
+   
+    <footer class="section p-3 bg-dark text-white">
+        <div class="text-center">&copy;  Copyright Rumah Makan Kita.  <?php echo date("Y"); ?> Ari Putra Utama</div>
+    </footer>
+</body>
+</html>
